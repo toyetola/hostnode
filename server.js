239e6ae46f85569f7056048aa8d96313946cc513
Oyetola <tooyetolaabidemi@gmail.com>
2020-01-14 14:27:18 +0100

    controllers models and routes added

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.use(bodyParser.json())
+const dbConfig = 'mongodb+srv://oyetola:oyetola24@notedb-qwcae.mongodb.net/test?retryWrites=true&w=majority';
+// const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+//connecting to the database
+mongoose.connect(dbConfig, {
+    useNewUrlParser : true
+});/* .then(()=>{
+    console.log("Successfully connected to the database")
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+}); */
+
+mongoose.connection.once('open', function(){
+      console.log('Conection has been made!');
+    }).on('error', function(error){
+        console.log('Error is: ', error);
 });
-server.listen();
+
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes"})
+});
+
+require('./app/routes/note.routes.js')(app);
+
+//listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
+
